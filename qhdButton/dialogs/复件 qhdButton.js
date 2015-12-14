ffcdf39e9a84a0545b35e3ccd58b827561c16dd3
方雨_Yu <304683191@qq.com>
2015-12-14 11:56:40 +0800

    CKEditor
    CKEditor 自定义插件

---
+++ qhdButton/dialogs/复件 qhdButton.js
@@ -0,0 +1,279 @@
+﻿/*
+ * qhd Ckeditor qhdButton.js v2.0
+ * create the time 2013.2.24
+*/
+
+function getIframe (name) 
+{
+	var iframe;
+    if (document.all){//IE
+		iframe = document.frames[name].document;
+    }else{//Firefox    
+		iframe = document.getElementById(name).contentDocument;
+    }
+	return iframe;
+}
+
+// get element by class
+function getByClass (className, parent) 
+{
+  parent || (parent=document);
+  var descendants=parent.getElementsByTagName('*'), i=-1, e, result=[];
+  while (e=descendants[++i]) {
+	((' '+(e['class']||e.className)+' ').indexOf(' '+className+' ') > -1) && result.push(e);
+  }
+  return result;
+}
+	
+//获取选中的文本内容
+function getSelectedText (editor) 
+{
+	var data="";  
+	var mySelection = editor.getSelection();
+	if (CKEDITOR.env.ie) {
+		mySelection.unlock(true);
+		data = mySelection.getNative().createRange().text;
+		mySelection.lock(true);
+	} else {
+		data = mySelection.getNative();
+	}
+	return data;
+}
+
+alert( '?' );
+
+/* S dialog */	
+CKEDITOR.dialog.add( 'qhdButton', function ( editor )
+{	
+	return {
+		title : '插入/编辑按钮',
+		minWidth : 500,
+		minHeight : 450,
+		buttons: [CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton],			
+		resizable: CKEDITOR.DIALOG_RESIZE_BOTH,	//对话框是否可以被调整
+		
+		contents :
+		[
+			{
+				id : 'button_info',
+				label : '超链接',
+				elements :
+				[					
+					// UI elements of the second tab will be defined here
+					{
+						type : 'text',
+						id : 'button_cont',
+						label : '文本内容',						
+						required : true,
+						setup : function( element )
+						{								
+							if ( getSelectedText(editor) && getSelectedText(editor) != '' ){
+								this.setValue( getSelectedText(editor) );
+							}else{
+								this.setValue( element.getText() );	
+							}					
+						},
+						commit : function( element )
+						{							  					
+							element.getFirst().setText( this.getValue() );	//修改span里面的文字内容
+							//alert( element.getFirst().getText() );
+						}
+					},
+					{
+						type : 'text',
+						id : 'button_title',
+						label : '提示内容',						
+						required : true,
+						setup : function( element )
+						{
+							this.setValue( element.getAttribute( "title" ) );
+						},
+						commit : function( element )
+						{							
+							element.setAttribute( 'title', this.getValue() );
+							//alert( element.getAttribute('title') );
+						}
+					},
+					{
+						type : 'text',
+						id : 'button_url',
+						label : '链接地址',						
+						required : true,
+						setup : function( element )
+						{
+							this.setValue( element.getAttribute( "href" ) );
+						},
+						commit : function( element )
+						{							
+							var buttonHref = this.getValue() ? this.getValue() : 'javascript:;';							
+							element.setAttribute( 'href', buttonHref );
+							element.setAttribute( 'data-cke-saved-href', buttonHref );
+							//alert( element.getAttribute('href') );
+						}
+					},
+					{
+						type : 'html',
+						html:'<span style="color:#666;">提示：若是电子邮箱，则按后面括号的格式在上栏中填入（maito:邮箱）</span>'
+					},					
+					{
+						type : 'checkbox',
+						id : 'button_newPage',
+						label : '新窗口打开',
+						'default' : false,
+						commit : function( element )
+						{							
+							if ( this.getValue() ){
+								element.setAttribute( 'target', '_blank' );
+							}else{
+								element.removeAttribute('target');
+							}							
+						}
+					},
+					{
+						type : 'text',
+						id : 'button_class',
+						className : 'button_class',
+						label : '按钮的class类名',
+						style : 'display:none; margin:-10px 0',
+						setup : function( element )
+						{		
+											
+							var buttonClass = element.getAttribute('class') ? element.getAttribute('class') : element.getAttribute('className') ;
+							this.setValue( buttonClass );
+							
+							//获取iframe的值。
+							var buttonSelectIframe = getIframe ('buttonSelectIframe'),
+								buttonBox = buttonSelectIframe.getElementById('button_box'),
+								buttonBoxItem = getByClass('button_box_item', buttonBox);
+							
+							for(var i=0; i<buttonBoxItem.length; i++){
+								var buttonElement = buttonBoxItem[i].getElementsByTagName('a')[0],
+									buttonClass = buttonElement.getAttribute('class') ? buttonElement.getAttribute('class') : buttonElement.getAttribute('className'); 
+								
+								//初始化所有的按钮box的class
+								for(var j=0; j<buttonBoxItem.length; j++){
+									buttonBoxItem[j].setAttribute('class','button_box_item');
+									buttonBoxItem[j].className = "button_box_item";	//for IE
+									//buttonBoxItem[j].setAttribute('className','button_box_item');	//for IE
+									
+								}
+								if( buttonClass == this.getValue() ){
+									buttonBoxItem[i].setAttribute('class','button_box_item item_select');
+									buttonBoxItem[i].className = "button_box_item item_select";	//for IE
+									// buttonBoxItem[i].setAttribute('className','button_box_item item_select'); //for IE
+									break;
+								}
+							}
+							
+						},
+						commit : function( element )
+						{				
+							element.setAttribute( 'class', this.getValue() );	
+						}
+					},
+					{
+						type : 'html',
+						id : 'buttonSelect',
+						html: 
+						'<div id="div1" style="width:640px; height:250px; background-color:#fff">' + 
+							'<iframe id="buttonSelectIframe" name="buttonSelectIframe" frameborder="0" style="width:640px; height:250px;" src ="' + CKEDITOR.basePath + 'plugins/qhdButton/dialogs/buttonSelect.html"></iframe>' + 
+						'</div>'
+					}
+				]
+			}
+			
+		],	//end of contents
+		
+		onShow: function(element)
+		{
+			//onShow event			
+			//alert( 'onShow event' )
+			var sel = editor.getSelection(),
+				element = sel.getStartElement();
+			
+			//alert( sel.getName() );
+			
+			if ( element )
+				element = element.getAscendant( 'a', true );
+			 
+			if ( !element || element.getName() != 'a' || element.data( 'cke-realelement' ) )
+			{
+				element = editor.document.createElement( 'a' );
+				this.insertMode = true;
+			}else{
+				this.insertMode = false;
+			}
+			this.element = element;
+			this.setupContent( this.element );
+		},
+		
+		onOk: function(element)
+		{
+			// onOk event
+			//alert( 'onOk event' )
+			var buttonSelectIframe = getIframe ('buttonSelectIframe');
+			//alert( buttonSelectIframe.getElementById('button_box').innerHTML );
+			
+			var dialog = this,
+				btn = this.element,
+				getSelectionContent = '',
+				button = editor.document.createElement( 'a' ),
+				span = editor.document.createElement( 'span' )				
+				;
+			
+			if ( this.insertMode ){	
+				//插入模式
+				var	buttonCont = this.getValueOf( 'button_info', 'button_cont' ),
+					buttonTitle = this.getValueOf('button_info', 'button_title'),
+					buttonUrl = this.getValueOf('button_info', 'button_url'),
+					buttonNewPage = this.getValueOf('button_info', 'button_newPage'),
+					buttonClass = this.getValueOf('button_info', 'button_class');
+					//buttonClass = buttonSelectIframe.getElementById('buttonClassValue').value;
+				
+				//alert( buttonClass );
+				
+				buttonUrl = buttonUrl ? buttonUrl : 'javascript:;';
+				getSelectionContent = buttonCont ? buttonCont : buttonUrl;
+				buttonTitle = buttonTitle ? buttonTitle : buttonCont;				
+				
+				if ( buttonNewPage ) { button.setAttribute( 'target', '_blank' ); }
+				if ( buttonClass ){
+					span.setHtml( getSelectionContent );				
+					button.append( span );
+					button.setAttribute( 'href', buttonUrl );
+					button.setAttribute( 'title', buttonTitle );
+					button.setAttribute( 'class', buttonClass );
+					editor.insertElement( button );
+				}else{
+					button.setAttribute( 'href', buttonUrl );
+					button.setAttribute( 'title', buttonTitle );
+					button.setHtml( getSelectionContent );
+					editor.insertElement( button );
+				}
+			}else{
+				//修改模式
+				//alert(btn.getChild(0).type == CKEDITOR.NODE_TEXT);
+				//alert(btn.getChild(0).type == CKEDITOR.NODE_ELEMENT && btn.getChild(0).getName() != 'span');
+				
+				if( (btn.getChild(0).type == CKEDITOR.NODE_TEXT) || (btn.getChild(0).type == CKEDITOR.NODE_ELEMENT && btn.getChild(0).getName() != 'span') ) 
+				{	
+					//alert( '需要添加一个span标签' );					
+					/*var btnText = btn.getHtml();
+					btn.setHtml( '<span>'+ btnText +'</span>' );*/
+					
+					span.setHtml( btn.getHtml() );
+					btn.setHtml( '' );					
+					btn.append( span );
+					
+				}
+				this.commitContent( btn );
+			}
+			
+		}	//end of onOK event
+		
+		
+	};
+	
+}); 
+
+/* E dialog */
---
+++ qhdList/plugin.js
@@ -0,0 +1,511 @@
+﻿(function()
+{
+	//获取企互动静态html编辑的div区域
+	function getQhdContent(editor)
+	{
+		var element = editor.getSelection().getStartElement();	//获取当前光标所在位置的第一个标签
+		
+		while ( element ){
+			var name = element.getName();
+			if (name == 'html') { break }//跳出循环
+			if ( /* name == 'div' &&  */element.hasClass('qhd-content') ){
+				return element;
+			}
+			element = element.getParent();
+		}
+		return null;
+	}
+	
+	//获取当前鼠标所在位置的p标签的html内容，并且删除原来的p
+	function getElement(editor)
+	{
+		var element = editor.getSelection().getStartElement();	//获取当前光标所在位置的第一个标签		
+
+		while ( element ){
+			var name = element.getName();
+			if ( name == 'body' ) { break }//跳出循环
+			if ( name == 'span' || name == 'strong' || name == 'p' || name == 'th' || name == 'td' || name =='div' || name =='ul' || name == 'h1' || name == 'h2' || name == 'h3' || name == 'h4' || name == 'h5' || name == 'h6'){				
+				return element;
+			}
+			element = element.getParent();
+		}
+		return null;
+	}
+	
+	//获取列表（ul）标签
+	function getUl( editor, element )
+	{
+		while ( element ){
+			var name = element.getName();
+			if ( name == 'body' ) { break }//跳出循环
+			if ( name == 'ul' ){
+				return element;
+			}
+			element = element.getParent();
+		}
+		return null;
+	}
+	
+	
+	CKEDITOR.plugins.qhdList =
+	{	
+		getUiTabs : function( editor )
+		{
+			var uiTabs = [];
+
+			// read UI tabs value from config
+			var configUiTabs = editor.config.scayt_uiTabs || "1,1,1";
+
+			// convert string to array
+			configUiTabs = configUiTabs.split( ',' );
+
+			// "About us" should be always shown for standard config
+			configUiTabs[3] = "1";
+
+			for ( var i = 0; i < 4; i++ ) {
+				uiTabs[i] = (typeof window.scayt != "undefined" && typeof window.scayt.uiTags != "undefined")
+								? (parseInt(configUiTabs[i],10) && window.scayt.uiTags[i])
+								: parseInt(configUiTabs[i],10);
+			}
+			return uiTabs;
+		} 
+		
+	};	
+	
+	var plugin = CKEDITOR.plugins.qhdList;	
+	
+	//处理下拉菜单的点击事件
+	function customList( editor, className )
+	{
+		var listHtml = '',
+			theElement = '',
+			elementName = '',
+			elementHtml = '',
+			parentElement = '';
+		
+		theElement = getElement(editor);	//鼠标当前位置的html标签
+		theElement = getUl(editor, theElement) ? getUl(editor, theElement) : theElement;	//判断是否为ul标签
+		elementName = theElement.getName(); //鼠标当前位置的html标签的名称
+		elementHtml = theElement.getHtml(); //鼠标当前位置的html标签的html内容
+		parentElement =  theElement.getParent();	//鼠标当前位置的父级html标签
+		
+		switch( elementName )
+		{				
+			case 'ul':
+				if( theElement.getAttribute('class') == className ){
+					removeQhdList( editor );
+				}else{
+					//修改自定义列表图标
+					theElement.setAttribute('class', className ); 
+				}
+			break;
+			
+			default:
+				//默认添加自定义列表代码段
+				if(elementHtml == '') elementHtml = '<br />';
+				listHtml = '<ul class="'+ className + '"><li>' + elementHtml +  '</li></ul>';
+				
+				if( getQhdContent(editor) != null ){
+					
+					if( (elementName == 'span') || (elementName == 'strong') ||  (elementName == 'p') ||  (elementName == 'h1') ||  (elementName == 'h2') ||  (elementName == 'h3') ||  (elementName == 'h4') ||  (elementName == 'h5') ||  (elementName == 'h6') ){
+						if ( theElement.hasNext() ){
+							if ( CKEDITOR.env.ie6Compat ){
+								//ie 6: 先删除，再添加					
+								theElement.remove();
+								editor.insertHtml(listHtml);
+							}else{
+								//非ie6: 先在下一个兄弟节点前添加，再删除
+								var listUl =  new CKEDITOR.dom.element( 'ul' ),
+									listLi =  new CKEDITOR.dom.element( 'li' )
+									;							
+								listLi.setHtml( elementHtml );
+								listUl.addClass( className );
+								listUl.append(listLi);
+								listUl.insertBefore( theElement.getNext() );
+								theElement.remove();
+							}
+							
+						}else{
+							//没有兄弟节点时，直接在父级最后面添加
+							parentElement.appendHtml( listHtml );
+							theElement.remove();	//删除原来所选择的html
+						
+						}
+						
+					}else if( (elementName == 'div') || (elementName == 'td') ||  (elementName == 'th') ){						
+						theElement.setHtml( listHtml );	//标签为div、td、th包住时，直接设置html为列表的html
+					}
+					
+				}	
+		}
+		
+	}
+	
+	//删除自定义列表函数
+	function removeQhdList( editor )
+	{
+		var listHtml = '',
+			theElement = '',
+			elementName = '',
+			elementHtml = '',
+			parentElement = '',
+			liHtml = '',
+			resultHtml = ''
+			;
+		
+		theElement = getElement(editor);	//鼠标当前位置的html标签
+		theElement = getUl(editor, theElement) ? getUl(editor, theElement) : theElement;	//判断是否为ul标签
+		elementName = theElement.getName(); //鼠标当前位置的html标签的名称
+		elementHtml = theElement.getHtml(); //鼠标当前位置的html标签的html内容
+		parentElement = theElement.getParent();	//鼠标当前位置的父级html标签
+		
+		if ( theElement.hasNext() ){
+			if ( CKEDITOR.env.ie6Compat ){
+				//ie 6: 先删除，再添加
+				for(var i=0; i<theElement.getChildCount(); i++){
+					liHtml = theElement.getChild(i).getHtml();
+					resultHtml += '<p>' + liHtml + '</p>';
+				}
+				theElement.remove();
+				editor.insertHtml( resultHtml );				
+				
+			}else{
+				//非IE6: 先在下一个兄弟节点前添加，再删除
+				for(var i=0; i<theElement.getChildCount(); i++){
+					var pElement =  new CKEDITOR.dom.element( 'p' );
+					
+					liHtml = theElement.getChild(i).getHtml();
+					pElement.setHtml( liHtml );				
+					pElement.insertBefore( theElement );	//删除情况比较特殊，插入在该元素之前。ie6 has bug
+				}
+				theElement.remove();
+			}
+			
+		}else{
+			//没有下一个兄弟节点时，直接在父级最后面添加，在删除
+			for(var i=0; i<theElement.getChildCount(); i++){
+				liHtml = theElement.getChild(i).getHtml();
+				resultHtml += '<p>' + liHtml + '</p>';
+			}
+			parentElement.appendHtml( resultHtml );
+			theElement.remove();
+		}
+		
+	}
+	
+	
+	// Add qhdList plugin.
+	CKEDITOR.plugins.add( 'qhdList',
+	{
+		requires : [ 'menubutton' ],
+		
+		init : function( editor )
+		{				
+			var menuGroup = 'qhdListButton';
+			editor.addMenuGroup( menuGroup );
+			// combine menu items to render
+			var uiMenuItems = {};			
+
+			// always added
+			uiMenuItems.ico_list_default =
+			{
+				label : '小黑点',
+				icon  : this.path + 'ico_list/ico_list_default.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, ''); }
+			};
+			uiMenuItems.ico_list_square =
+			{
+				label : '小方块',
+				icon  : this.path + 'ico_list/ico_list_square.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'square'); }
+			};
+			uiMenuItems.ico_list_circle =
+			{
+				label : '小圆圈',
+				icon  : this.path + 'ico_list/ico_list_circle.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'circle'); }
+			};
+			uiMenuItems.ico_list_delta =
+			{
+				label : '小三角形',
+				icon  : this.path + 'ico_list/ico_list_delta.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-delta'); }
+			};
+			uiMenuItems.ico_list_dot =
+			{
+				label : '向右反向标',
+				icon  : this.path + 'ico_list/ico_list_dot.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-dot'); }
+			};
+			uiMenuItems.ico_list_favicon =
+			{
+				label : '五角星',
+				icon  : this.path + 'ico_list/ico_list_favicon.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-favicon'); }
+			};
+			uiMenuItems.ico_list_download =
+			{
+				label : '向下箭头',
+				icon  : this.path + 'ico_list/ico_list_download.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-download'); }
+			};
+			uiMenuItems.ico_list_check =
+			{
+				label : '打钩',
+				icon  : this.path + 'ico_list/ico_list_check.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-check'); }
+			};
+			uiMenuItems.ico_list_arrow =
+			{
+				label : '小箭头',
+				icon  : this.path + 'ico_list/ico_list_arrow.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-arrow'); }
+			};
+			uiMenuItems.ico_list_del =
+			{
+				label : '红叉',
+				icon  : this.path + 'ico_list/ico_list_del.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-del'); }
+			};
+			uiMenuItems.ico_list_pen =
+			{
+				label : '笔',
+				icon  : this.path + 'ico_list/ico_list_pen.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-pen'); }
+			};
+			uiMenuItems.ico_list_light =
+			{
+				label : '提示',
+				icon  : this.path + 'ico_list/ico_list_light.gif',					
+				group : menuGroup,
+				onClick : function() { customList( editor, 'iconlist iconlist-light'); }
+			};
+
+			editor.addMenuItems( uiMenuItems );
+
+			editor.ui.add( 'qhdList', CKEDITOR.UI_MENUBUTTON,
+			{
+				label : '编辑自定义列表',
+				title : '编辑自定义列表',
+				icon  : this.path + 'qhdList.gif',
+				//className : 'cke_button_list',
+				modes : { wysiwyg : 1 },				
+				onMenu : function()
+				{
+					return {
+						ico_list_default  : CKEDITOR.TRISTATE_OFF,
+						ico_list_square   : CKEDITOR.TRISTATE_OFF,
+						ico_list_circle   : CKEDITOR.TRISTATE_OFF,
+						ico_list_delta    : CKEDITOR.TRISTATE_OFF,
+						ico_list_dot      : CKEDITOR.TRISTATE_OFF,
+						ico_list_favicon  : CKEDITOR.TRISTATE_OFF,
+						ico_list_download : CKEDITOR.TRISTATE_OFF,
+						ico_list_check    : CKEDITOR.TRISTATE_OFF,
+						ico_list_arrow    : CKEDITOR.TRISTATE_OFF,
+						ico_list_del      : CKEDITOR.TRISTATE_OFF,
+						ico_list_pen      : CKEDITOR.TRISTATE_OFF,
+						ico_list_light    : CKEDITOR.TRISTATE_OFF
+					};
+				}
+			});
+			
+			editor.addCommand('removeQhdList',
+			{
+				exec : function( editor )
+				{
+					removeQhdList( editor );
+				}
+			});
+			
+			
+			/* S 处理右键上下文菜单 */
+			// If the "menu" plugin is loaded, register the menu items.
+			if ( editor.addMenuItems )
+			{
+				//Register qhdList group;
+				editor.addMenuGroup("qhdList", 208);
+
+				editor.addMenuItems(
+				{	
+					mofifyQhdList :
+					{
+						label : '修改自定义列表',
+						//command: 'customList',
+						group : 'qhdList',
+						icon: this.path + 'qhdList.gif',
+						order : 1,
+						getItems : function()
+						{
+							return {
+								mofifyQhdList_01 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_02 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_03 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_04 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_05 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_06 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_07 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_08 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_09 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_10 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_11 : CKEDITOR.TRISTATE_OFF,
+								mofifyQhdList_12 : CKEDITOR.TRISTATE_OFF
+							};
+						}
+					},
+					mofifyQhdList_01 :
+					{
+						label : '小黑点',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_default.gif',
+						onClick : function() { customList( editor, ''); }
+					},
+					mofifyQhdList_02 :
+					{
+						label : '小方块',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_square.gif',
+						onClick : function() { customList( editor, 'square'); }
+					},
+					mofifyQhdList_03 :
+					{
+						label : '小圆圈',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_circle.gif',
+						onClick : function() { customList( editor, 'circle'); }
+					},
+					mofifyQhdList_04 :
+					{
+						label : '小三角形',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_delta.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-delta'); }
+					},
+					mofifyQhdList_05 :
+					{
+						label : '向右反向标',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_dot.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-dot'); }
+					},
+					mofifyQhdList_06 :
+					{
+						label : '五角星',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_favicon.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-favicon'); }
+					},
+					mofifyQhdList_07 :
+					{
+						label : '向下箭头',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_download.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-download'); }
+					},
+					mofifyQhdList_08 :
+					{
+						label : '打勾',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_check.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-check'); }
+					},
+					mofifyQhdList_09 :
+					{
+						label : '小箭头',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_arrow.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-arrow'); }
+					},
+					mofifyQhdList_10 :
+					{
+						label : '红叉',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_del.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-del'); }
+					},
+					mofifyQhdList_11 :
+					{
+						label : '笔',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_pen.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-pen'); }
+					},
+					mofifyQhdList_12 :
+					{
+						label : '提示',
+						group : 'qhdList',
+						icon: this.path + 'ico_list/ico_list_light.gif',
+						onClick : function() { customList( editor, 'iconlist iconlist-light'); }
+					},
+					
+					removeQhdList :
+					{
+						label : '删除自定义列表',
+						command: 'removeQhdList',
+						group : 'qhdList',
+						icon: this.path + 'removeQhdList.gif',						
+						order : 20
+					}
+				});
+			}
+			
+			// If the "contextmenu" plugin is loaded, register the listeners.
+			if ( editor.contextMenu )
+			{
+				editor.contextMenu.addListener( function( element, selection )
+				{	
+					if ( !element || element.isReadOnly() )
+						return null;
+					
+					while ( element )
+					{
+						var name = element.getName();						
+						if ( name == 'ul'){
+							editor.contextMenu.removeAll();	//删除所有的上下文菜单
+							return {
+								mofifyQhdList : CKEDITOR.TRISTATE_OFF,
+								removeQhdList : CKEDITOR.TRISTATE_OFF
+							};
+						}
+						element = element.getParent();
+					}
+					return null;
+					
+				});
+			}			
+			/* E 处理右键上下文菜单 */
+			
+			/* S 设置自定义列表的当前状态 */
+			editor.on( 'selectionChange', function( e )
+			{
+				//editor.getCommand('qhdList').setState(CKEDITOR.TRISTATE_OFF);
+				var element = e.data.selection.getStartElement();	//获取当前选择的标签
+				//alert( element );
+				while ( element )
+				{
+					var name = element.getName();
+					if ( name == 'ul' ){						
+						editor.getCommand('bulletedlist').setState(CKEDITOR.TRISTATE_OFF);	//取消ul的当前状态
+						//editor.getCommand('qhdList').setState(CKEDITOR.TRISTATE_ON);		//设置自定义列表的当前状态
+					}
+					element = element.getParent();
+				}
+				return null;
+				
+			})
+			/* E 设置自定义列表的当前状态 */
+		
+		}	//end of init
+	});
+	
+})();
